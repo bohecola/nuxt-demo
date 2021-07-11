7f5358cba275829a18a4d00cd3f94fcd2e55939c
bohecola <bohecola@outlook.com>
2021-07-11 23:55:51 +0800

    nuxt

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>A user -- ID: {{$route.params.id}}</h1>
+</template>
+
+<script>
+export default {
+  validate(data) {
+    return /^\d+$/.test(data.params.id)
+  }
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>请输入一个id ，并按下按钮。</p>
+</template>
